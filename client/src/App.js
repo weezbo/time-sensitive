95474895dca9e42e01eea7a826c12efc070eed5c
Max Muir <mrmaxmuir@gmail.com>
2020-11-13 08:55:34 -0500

    Now have working login/redirect. Need to block out add function and use user token.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,42 @@
 import React, { Component } from "react";
 import { Switch, Route, Link } from "react-router-dom";
+import AuthService from "./services/auth.service";
+import Login from "./components/login.component";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 import AddTaskLog from "./components/AddTaskLog.component";
+import Profile from "./components/profile.component";
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.logOut = this.logOut.bind(this);
+
+        this.state = {
+            currentUser: undefined,
+        }
+    }
+
+    componentDidMount() {
+        const user = AuthService.getCurrentUser();
+
+        if(user) {
+            this.setState({
+                currentUser: user,
+            })
+        }
+    }
+
+    logOut() {
+        AuthService.logout();
+    }
+
     render() {
+
+        const { currentUser } = this.state;
+
         return (
             <div>
                 <nav className="navbar navbar-expand navbar-dark bg-dark">
@@ -20,11 +50,35 @@ class App extends Component {
                             </Link>
                         </li>
                     </div>
+                    { currentUser ? (
+                        <div className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link to={"/profile"} className="nav-link">
+                                    {currentUser.username}
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/login" className="nav-link" onClick={this.logOut}>
+                                    LogOut
+                                </a>
+                            </li>
+                        </div>
+                    ) : (
+                        <div className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link to={"/login"} className="nav-link">
+                                    Login
+                                </Link>
+                            </li>
+                        </div>
+                    )}
                 </nav>
 
                 <div className="container mt-3">
                     <Switch>
+                        <Route exact path="/login" component={Login} />
                         <Route exact path="/" component={AddTaskLog} />
+                        <Route exact path="/profile" component={Profile} />
                     </Switch>
                 </div>
             </div>
--- server/app/controllers/auth.controller.js
+++ server/app/controllers/auth.controller.js
@@ -3,7 +3,7 @@ const config = require("../config/auth.config");
 const User = db.users;
 
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+// const bcrypt = require("bcryptjs");
 
 
 exports.signin = (req, res) => {
@@ -16,23 +16,21 @@ exports.signin = (req, res) => {
             if (!user) {
                 return res.status(404).send({ message: "User Not found." });
             }
-
-            const passwordIsValid = bcrypt.compareSync(
-                req.body.password,
-                user.password
-            );
-
+            // Removed bcrypt from here. Very dangerous!
+            const passwordIsValid =
+                req.body.password ===
+                user.password;
             if (!passwordIsValid) {
                 return res.status(401).send({
                     accessToken: null,
-                    message: "Invalid Password!"
+                    message: "Invalid Password!!!"
                 });
             }
 
             const token = jwt.sign({ id: user.id }, config.secret, {
                 expiresIn: 86400 // 24 hours
             });
-            
+
             return res.status(200).send({
                 id: user.id,
                 username: user.username,
